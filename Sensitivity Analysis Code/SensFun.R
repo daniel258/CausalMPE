e7902eb17595ff81a3c68429bb95d8446a8014d6
daniel258 <your_email@example.com>
2020-01-05 08:39:35 +0200

    SensFun to have the etas in multiplicative form

--- Sensitivity Analysis Code/SensFun.R
+++ Sensitivity Analysis Code/SensFun.R
@@ -8,8 +8,8 @@ SensFun <- function(coef1, coef2, eta1, eta2, coef.covmat)
 {
   RR1 <- exp(coef1)
   RR2 <- exp(coef2)
-  point1 <- RR1/eta1 
-  point2 <- RR2/eta2
+  point1 <- RR1 * eta1 
+  point2 <- RR2 * eta2
   point.diff <- point1 - point2
   my.sd <- sqrt(c(point1, -point2)%*%coef.covmat%*%c(point1, -point2))
   ci.l <- point.diff - 1.96 * my.sd
