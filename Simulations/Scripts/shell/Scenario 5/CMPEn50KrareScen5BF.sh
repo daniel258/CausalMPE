e431af380f232cfceeb11a61b0467d730b74e263
daniel258 <your_email@example.com>
2020-09-23 11:07:02 +0300

    added new shells

---
+++ Simulations/Scripts/shell/Scenario 5/CMPEn50KrareScen5BF.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -n 1                    # Number of cores requested
+#SBATCH -t 5:00:00                    # Runtime in minutes
+                                # Or use HH:MM:SS or D-HH:MM:SS, instead of just number of minutes
+#SBATCH -p short                # Partition (queue) to submit to
+#SBATCH --mem-per-cpu=8G        # 8 GB memory needed (memory PER CORE)
+#SBATCH --open-mode=append      # append adds to outfile, truncate deletes first
+### In filenames, %j=jobid, %a=index in job array
+#SBATCH -o CMPEn50KrareScen5BF.R.out               # Standard out goes to this file
+#SBATCH -e CMPEn50KrareScen5BF.R.err               # Standard err goes to this file
+#SBATCH --mail-type=END         # Mail when the job ends  
+#write command-line commands below this line
+module load gcc/6.2.0 R/3.5.1
+Rscript CMPEn50KrareScen5BF.R
